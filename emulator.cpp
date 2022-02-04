6dd25d94b122e07100e145c65edc80f3621c9960
ChrisFischer-MTA <ChrisFischer-MTA@users.noreply.github.com>
2022-02-03 22:41:05 -0500

    Start of Emulator.cpp

---
+++ emulator.cpp
@@ -0,0 +1,128 @@
+// Christopher Fischerr
+// 03FEB2022
+// Emulator Specific Information
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+// So the idea here is we can define generally what an instruction holds, and then
+// read from a file with metadata on the instructions, and match from there.
+class Instruction
+{
+};
+
+class EmulatedCPU
+{
+	public:
+		// The Program Counter value. During the instruction time of an instruction this is the address of the
+		// instruction word. The address of the instruction that occurs during the next instruction time 
+		// is determined by assigning a value to PC during an instruction time. If no value is assigned to PC
+		// during instruction time by any pseudocode statement, it is automatically incremented by four.
+		// Read more info when working with this.
+		uint32_t pc;
+		// REGISTERS
+
+		uint32_t registers[32];		
+		
+		std::string getName(int offset)
+		{
+			if ((offset ==	0))	return "zero";
+			if (offset ==	1)	return "at";
+			if (offset ==	2)	return "v0";
+			if (offset ==	3)	return "v1";
+			if (offset ==	4)	return "a0";
+			if (offset ==	5)	return "a1";
+			if (offset ==	6)	return "a2";
+			if (offset ==	7)	return "a3";
+			if (offset ==	8)	return "t0";
+			if (offset ==	9)	return "t1";
+			if (offset ==	10)	return "t2";
+			if (offset ==	11)	return "t3";
+			if (offset ==	12)	return "t4";
+			if (offset ==	13)	return "t5";
+			if (offset ==	14)	return "t6";
+			if (offset ==	15)	return "t7";
+			if (offset ==	16)	return "s0";
+			if (offset ==	17)	return "s1";
+			if (offset ==	18)	return "s2";
+			if (offset ==	19)	return "s3";
+			if (offset ==	20)	return "s4";
+			if (offset ==	21)	return "s5";
+			if (offset ==	22)	return "s6";
+			if (offset ==	23)	return "s7";
+			if (offset ==	24)	return "t8";
+			if (offset ==	25)	return "t9";
+			if (offset ==	26)	return "k0";
+			if (offset ==	27)	return "k1";
+			if (offset ==	28)	return "gp";
+			if (offset ==	29)	return "sp";
+			if (offset ==	30)	return "fp";
+			if (offset ==	31)	return "ra";
+			return "NULL";
+		}
+		
+		// pg. 40
+		void runInstruction(uint32_t opcode)
+		{
+			
+			if((opcode & 0xfc000000) == 0)
+			{
+				printf("Special\n");
+				printf("%u\n", (opcode & 0xfc));
+				
+				// ADD, MIPS 1
+				if((opcode & 0x7ff) == 0x20)
+				{
+					printf("ADD\n");
+				}				
+				// AND, MIPS 1
+				if((opcode & 0x7ff) == 0x24)
+				{
+					printf("AND\n");
+				}
+				// ADDU
+				if((opcode & 0x7ff) == 0x21)
+				{
+					printf("ADDU\n");
+				}
+			}
+			
+			
+			// ADDI, MIPS 1
+			if((opcode & 0xfc000000) == 0x20000000)
+			{
+				printf("ADDI\n");
+			}
+			
+			// ADDI, MIPS 1
+			if((opcode & 0xfc000000) == 0x24000000)
+			{
+				printf("ADDIU\n");
+			}
+			
+			
+			
+			pc += 4;
+			
+			
+			
+		}
+};
+
+
+int main(int argn, char ** args)
+{
+	EmulatedCPU electricrock;
+	printf("%d %s\n", 31, electricrock.getName(31).c_str());
+
+	// This is a valid opcode extracted from a program
+	// memonic: and $v0, $v1, $v0
+	electricrock.runInstruction(0x00621024);
+	
+	// memonic: addu $v0, $s5, $v0
+	electricrock.runInstruction(0x02a21021);
+	
+}
+

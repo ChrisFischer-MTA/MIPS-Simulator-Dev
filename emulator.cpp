54d30228a8876d41c02c65504d47167eeaab51cf
Chris Fischer <christopherrfischer@outlook.com>
2022-02-15 17:00:39 -0500

    added add, addi, addiu, addu

--- emulator.cpp
+++ emulator.cpp
@@ -9,7 +9,6 @@
 
 const short int MIP_ISA_32 = 1;
 
-
 const short int MIPSI = 1;
 const short int MIPSII = 2;
 const short int MIPSIII = 3;
@@ -19,16 +18,9 @@ const short int RTYPE = 1;
 const short int ITYPE = 2;
 const short int JTYPE = 3;
 
-class instruction
-{
-public:
-	short int targetVersion;
-	short int targetISA;
-	short int instructionType;
-	void* funcPntr;
-	char* memonic;
-};
 
+// Exception Types
+const short int IntegerOverflow = 1;
 
 class EmulatedCPU
 {
@@ -40,8 +32,147 @@ class EmulatedCPU
 		// Read more info when working with this.
 		uint32_t pc;
 		// REGISTERS
+
+		// Hacky solution I do not understand which allows us to have a function table.
+		typedef void (EmulatedCPU::* const funct)(uint32_t);
 		
-		instruction* itypes;
+		// A list of functions that are rtypes index by their ALU code.
+		// Indexed by their ALU code.
+		const EmulatedCPU::funct EmulatedCPU::inst_handlers_rtypes[64] = {
+			& EmulatedCPU::unimplemented, // 0
+			& EmulatedCPU::unimplemented, // 1
+			& EmulatedCPU::unimplemented, // 2
+			& EmulatedCPU::unimplemented, // 3
+			& EmulatedCPU::unimplemented, // 4
+			& EmulatedCPU::unimplemented, // 5
+			& EmulatedCPU::unimplemented, // 6
+			& EmulatedCPU::unimplemented, // 7
+			& EmulatedCPU::unimplemented, // 8
+			& EmulatedCPU::unimplemented, // 9
+			& EmulatedCPU::unimplemented, // 10
+			& EmulatedCPU::unimplemented, // 11
+			& EmulatedCPU::unimplemented, // 12
+			& EmulatedCPU::unimplemented, // 13
+			& EmulatedCPU::unimplemented, // 14
+			& EmulatedCPU::unimplemented, // 15
+			& EmulatedCPU::unimplemented, // 16
+			& EmulatedCPU::unimplemented, // 17
+			& EmulatedCPU::unimplemented, // 18
+			& EmulatedCPU::unimplemented, // 19
+			& EmulatedCPU::unimplemented, // 20
+			& EmulatedCPU::unimplemented, // 21
+			& EmulatedCPU::unimplemented, // 22
+			& EmulatedCPU::unimplemented, // 23
+			& EmulatedCPU::unimplemented, // 24
+			& EmulatedCPU::unimplemented, // 25
+			& EmulatedCPU::unimplemented, // 26
+			& EmulatedCPU::unimplemented, // 27
+			& EmulatedCPU::unimplemented, // 28
+			& EmulatedCPU::unimplemented, // 29
+			& EmulatedCPU::unimplemented, // 30
+			& EmulatedCPU::unimplemented, // 31
+			& EmulatedCPU::add, // 32
+			& EmulatedCPU::addu, // 33
+			& EmulatedCPU::unimplemented, // 34
+			& EmulatedCPU::unimplemented, // 35
+			& EmulatedCPU::unimplemented, // 36
+			& EmulatedCPU::unimplemented, // 37
+			& EmulatedCPU::unimplemented, // 38
+			& EmulatedCPU::unimplemented, // 39
+			& EmulatedCPU::unimplemented, // 40
+			& EmulatedCPU::unimplemented, // 41
+			& EmulatedCPU::unimplemented, // 42
+			& EmulatedCPU::unimplemented, // 43
+			& EmulatedCPU::unimplemented, // 44
+			& EmulatedCPU::unimplemented, // 45
+			& EmulatedCPU::unimplemented, // 46
+			& EmulatedCPU::unimplemented, // 47
+			& EmulatedCPU::unimplemented, // 48
+			& EmulatedCPU::unimplemented, // 49
+			& EmulatedCPU::unimplemented, // 50
+			& EmulatedCPU::unimplemented, // 51
+			& EmulatedCPU::unimplemented, // 52
+			& EmulatedCPU::unimplemented, // 53
+			& EmulatedCPU::unimplemented, // 54
+			& EmulatedCPU::unimplemented, // 55
+			& EmulatedCPU::unimplemented, // 56
+			& EmulatedCPU::unimplemented, // 57
+			& EmulatedCPU::unimplemented, // 58
+			& EmulatedCPU::unimplemented, // 59
+			& EmulatedCPU::unimplemented, // 60
+			& EmulatedCPU::unimplemented, // 61
+			& EmulatedCPU::unimplemented, // 62
+			& EmulatedCPU::unimplemented, // 63
+		};
+
+		// A list of functions that are other types then rtypes. 
+		// Index by their opcode.
+		const EmulatedCPU::funct EmulatedCPU::inst_handlers_otypes[64] = {
+			& EmulatedCPU::unimplemented, // 0
+			& EmulatedCPU::unimplemented, // 1
+			& EmulatedCPU::unimplemented, // 2
+			& EmulatedCPU::unimplemented, // 3
+			& EmulatedCPU::unimplemented, // 4
+			& EmulatedCPU::unimplemented, // 5
+			& EmulatedCPU::unimplemented, // 6
+			& EmulatedCPU::unimplemented, // 7
+			& EmulatedCPU::addi, // 8
+			& EmulatedCPU::addiu, // 9
+			& EmulatedCPU::unimplemented, // 10
+			& EmulatedCPU::unimplemented, // 11
+			& EmulatedCPU::unimplemented, // 12
+			& EmulatedCPU::unimplemented, // 13
+			& EmulatedCPU::unimplemented, // 14
+			& EmulatedCPU::unimplemented, // 15
+			& EmulatedCPU::unimplemented, // 16
+			& EmulatedCPU::unimplemented, // 17
+			& EmulatedCPU::unimplemented, // 18
+			& EmulatedCPU::unimplemented, // 19
+			& EmulatedCPU::unimplemented, // 20
+			& EmulatedCPU::unimplemented, // 21
+			& EmulatedCPU::unimplemented, // 22
+			& EmulatedCPU::unimplemented, // 23
+			& EmulatedCPU::unimplemented, // 24
+			& EmulatedCPU::unimplemented, // 25
+			& EmulatedCPU::unimplemented, // 26
+			& EmulatedCPU::unimplemented, // 27
+			& EmulatedCPU::unimplemented, // 28
+			& EmulatedCPU::unimplemented, // 29
+			& EmulatedCPU::unimplemented, // 30
+			& EmulatedCPU::unimplemented, // 31
+			& EmulatedCPU::unimplemented, // 32
+			& EmulatedCPU::unimplemented, // 33
+			& EmulatedCPU::unimplemented, // 34
+			& EmulatedCPU::unimplemented, // 35
+			& EmulatedCPU::unimplemented, // 36
+			& EmulatedCPU::unimplemented, // 37
+			& EmulatedCPU::unimplemented, // 38
+			& EmulatedCPU::unimplemented, // 39
+			& EmulatedCPU::unimplemented, // 40
+			& EmulatedCPU::unimplemented, // 41
+			& EmulatedCPU::unimplemented, // 42
+			& EmulatedCPU::unimplemented, // 43
+			& EmulatedCPU::unimplemented, // 44
+			& EmulatedCPU::unimplemented, // 45
+			& EmulatedCPU::unimplemented, // 46
+			& EmulatedCPU::unimplemented, // 47
+			& EmulatedCPU::unimplemented, // 48
+			& EmulatedCPU::unimplemented, // 49
+			& EmulatedCPU::unimplemented, // 50
+			& EmulatedCPU::unimplemented, // 51
+			& EmulatedCPU::unimplemented, // 52
+			& EmulatedCPU::unimplemented, // 53
+			& EmulatedCPU::unimplemented, // 54
+			& EmulatedCPU::unimplemented, // 55
+			& EmulatedCPU::unimplemented, // 56
+			& EmulatedCPU::unimplemented, // 57
+			& EmulatedCPU::unimplemented, // 58
+			& EmulatedCPU::unimplemented, // 59
+			& EmulatedCPU::unimplemented, // 60
+			& EmulatedCPU::unimplemented, // 61
+			& EmulatedCPU::unimplemented, // 62
+			& EmulatedCPU::unimplemented, // 63
+		};
 
 		uint32_t registers[32];
 		int32_t rs; // 1st Source
@@ -50,18 +181,143 @@ class EmulatedCPU
 		int32_t immediate; // Immediate
 		int32_t signedImmediate; // Immediate
 		int32_t mipsTarget = 1;
+		bool debugPrint = true;
 
-		/*
-		Where I'm stuck.
-		I'd really like to make a function pointer table, indexing each of the operations by it's OPCODE.
-		But I can't get C++ to let me declare this. 
-		*/
 
 		EmulatedCPU()
 		{
-			itypes = new instruction[64];
+			int i;
+			pc = 0;
+			for (i = 0; i < 32; i++)
+				registers[i] = 0;		
+
 		}
 
+		// This function is a hacky way for us to freeze in the debug console which will be replaced
+		// by a system call in the future.
+		void generallyPause()
+		{
+			while (1)
+			{
+				// do nothing
+			}
+		}
+
+		void signalException(int excpt)
+		{
+			printf("Exception occured! [%d]\n", excpt);
+			while (1)
+			{
+				// do nothing
+			}
+		}
+
+		void EmulatedCPU::unimplemented(uint32_t opcode)
+		{
+			printf("\n\nPLEASE HELP ME YOU CALLED AN UNIMPLEMEND HANDLER\n\n");
+			printf("TIME TO DIE");
+			generallyPause();
+			return;
+		}
+
+		// This is the ADD function. Opcode of 0b000000 and ALU code of 0b100 000
+		// TODO: Test with negative values.
+		void EmulatedCPU::add(uint32_t opcode)
+		{
+
+			if(mipsTarget < 1)
+			{
+				printf("Invalid mips target for ADD\n");
+			}
+
+			if(debugPrint)
+			{
+				printf("ADD %s, %s, %s\n", getName(rd).c_str(), getName(rs).c_str(), getName(rt).c_str());
+			}
+			
+			int32_t temp = registers[rs] + registers[rt];
+
+			// Check for an overflow
+			if (((registers[rs] > 0) && (registers[rt] > 0) && (temp < 0)) ||
+					((registers[rs] < 0) && (registers[rt] < 0) && (temp > 0)))
+			{
+				signalException(IntegerOverflow);
+			}
+
+			registers[rd] = temp;
+
+		}
+		void EmulatedCPU::addi(uint32_t opcode)
+		{
+
+			if (mipsTarget < 1)
+			{
+				printf("Invalid mips target for ADDI\n");
+			}
+
+			if (debugPrint)
+			{
+				printf("ADDI %s, %s, %d\n", getName(rs).c_str(), getName(rt).c_str(), signedImmediate);
+			}
+
+			int32_t temp = registers[rs] + signedImmediate;
+
+			// Check for an overflow
+			if (((registers[rs] > 0) && (signedImmediate > 0) && (temp < 0)) ||
+				((registers[rs] < 0) && (signedImmediate < 0) && (temp > 0)))
+			{
+				signalException(IntegerOverflow);
+			}
+
+			registers[rt] = temp;
+
+		}
+		void EmulatedCPU::addiu(uint32_t opcode)
+		{
+
+			if (mipsTarget < 1)
+			{
+				printf("Invalid mips target for ADDIU\n");
+			}
+
+			if (debugPrint)
+			{
+				printf("ADDIU %s, %s, %d\n", getName(rs).c_str(), getName(rt).c_str(), signedImmediate);
+			}
+
+			int32_t temp = registers[rs] + signedImmediate;			
+			registers[rt] = temp;
+
+		}
+		void EmulatedCPU::addu(uint32_t opcode)
+		{
+			if (mipsTarget < 1)
+			{
+				printf("Invalid mips target for ADDU\n");
+			}
+
+			if (debugPrint)
+			{
+				printf("ADDU %s, %s, %s\n", getName(rd).c_str(), getName(rs).c_str(), getName(rt).c_str());
+			}
+
+			registers[rd] = registers[rs] + registers[rt];
+		}
+
+
+		void EmulatedCPU::registerDump()
+		{
+			int i;
+
+			printf("=================    CPU STATE:    =================\n");
+			printf("PC: [0%x]\n", pc);
+
+			printf("=================DUMPING REGISTERS:=================\n");
+			for (i = 0; i < 32; i++)
+			{
+				printf("%s -> %u\n", getName(i).c_str(), registers[i]);
+			}
+		}
 		std::string getName(int offset)
 		{
 			if ((offset ==	0))	return "zero";
@@ -99,35 +355,29 @@ class EmulatedCPU
 			return "NULL";
 		}
 		
-		void addiu(uint32_t rs, uint32_t rt, uint32_t immediate)
-		{
-			
-
-		}
-
 		// pg. 40
 		void runInstruction(uint32_t opcode)
 		{
 			// First, let's determine the instruction type.
-
+			rs = (opcode & 0x3E00000) >> 21;
+			rt = (opcode & 0x1F0000) >> 16;
+			rd = (opcode & 0xf800) >> 11;
+			immediate = opcode & 0xffff;
+			signedImmediate = opcode & 0xffff;
+			
 			// If the upper 26-31 bits are set to zero, then, we have an R-Type instruction
 			if ((opcode & 0xfc000000) == 0)
 			{
-				rs = (opcode & 0x3E00000) >> 21;
-				rt = (opcode & 0x1F0000) >> 16;
-				rd = (opcode & 0xf800) >> 11;
-
-				
+				// Essentially, this is a list of r type functions indexed by opcode.
+				printf("Rtype [%d]\n", (opcode & 0b111111));
+				(this->*inst_handlers_rtypes[(opcode & 0b111111)])(opcode);
+							
 			}
-			
-
-
-
-
-			
-			immediate = opcode & 0xffff;
-				
-			
+			else
+			{
+				printf("Otype [%d]\n", ((opcode & 0xfc000000) >> 26));
+				(this->*inst_handlers_otypes[(opcode & 0xfc000000) >> 26])(opcode);
+			}			
 			pc += 4;			
 		}
 };
@@ -141,15 +391,19 @@ int main(int argn, char ** args)
 
 	// This is a valid opcode extracted from a program
 	// memonic: and $v0, $v1, $v0
-	electricrock->runInstruction(0x00621024);
+	//electricrock->runInstruction(0x00621024);
 	
+	electricrock->registers[2] = 1337;
+	electricrock->registers[21] = 8008;
+
+
 	// memonic: addu $v0, $s5, $v0
 	electricrock->runInstruction(0x02a21021);
 
 	// memonic: addiu $a1,$zero, 1
 	electricrock->runInstruction(0x24050001);
 
-
+	electricrock->registerDump();
 	// Just a lazy way to stop things from progressing
 	char* str_space = (char*) malloc(sizeof(char) * 1024);
 	scanf("%s", str_space);

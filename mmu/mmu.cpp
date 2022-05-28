e1e57aeb908c3bc72823db55205bfbb9c1f3709d
SeanAKemp <sean.a.kemp@gmail.com>
2022-05-28 10:59:21 -0400

    more changes

--- mmu/mmu.cpp
+++ mmu/mmu.cpp
@@ -67,7 +67,7 @@ MMU::MMU(bool is64bit, BinaryNinja::BinaryView* bv)
 	}
 			
 
-		*/
+		*/f
 }
 
 MMU::MMU()
@@ -77,6 +77,7 @@ MMU::MMU()
 
 void MMU::secSort()
 {
+	/*
 	ArrayList<section> sorted = ArrayList<section>(allSections.size);
 	section least = allSections[0];
 	for (int j = 0;j < allSections.size;j++)
@@ -97,6 +98,7 @@ void MMU::secSort()
 	{
 		allSections[i] = sorted[i];
 	}
+	*/
 	return;
 		
 }

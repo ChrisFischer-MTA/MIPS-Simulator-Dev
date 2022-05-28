8fa6292cd229a7a19a7a49311fe126c89e76efaf
Chris Fischer <christopherrfischer@outlook.com>
2022-05-28 12:46:43 -0400

    Revert "commenting out stuf"
    This reverts commit 2aa7863782bc0e9d4b3e8eebfc60131c10a1d306.

--- mmu/mmu.cpp
+++ mmu/mmu.cpp
@@ -27,7 +27,6 @@ void generallyPause();
 MMU::MMU(bool is64bit, BinaryNinja::BinaryView* bv)
 {
 	// At some point need to check bv->platform->architecture for is64bit bool
-	/*
 	auto buttsegs = bv->GetSegments();
 	segments = ArrayList<segment>(buttsegs.size());
 	this->is64Bit = is64bit;
@@ -67,7 +66,7 @@ MMU::MMU(bool is64bit, BinaryNinja::BinaryView* bv)
 	}
 			
 
-		*/
+		
 }
 
 MMU::MMU()
